322776f2ca5dc6074e7a20d51bf946755b9bd14d
syphyr <syphyr@gmail.com>
2019-11-01 02:52:57 +0100

    Update fingerprint to T813XXS2BSJ3

--- lineage.mk
+++ lineage.mk
@@ -14,5 +14,5 @@ PRODUCT_GMS_CLIENTID_BASE := android-samsung
 # Use the latest approved GMS identifiers
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=gts210vewifixx \
-    BUILD_FINGERPRINT=samsung/gts210vewifixx/gts210vewifi:7.0/NRD90M/T813XXS2BSG1:user/release-keys \
-    PRIVATE_BUILD_DESC="gts210vewifixx-user 7.0 NRD90M T813XXS2BSG1 release-keys"
+    BUILD_FINGERPRINT=samsung/gts210vewifixx/gts210vewifi:7.0/NRD90M/T813XXS2BSJ3:user/release-keys \
+    PRIVATE_BUILD_DESC="gts210vewifixx-user 7.0 NRD90M T813XXS2BSJ3 release-keys"
